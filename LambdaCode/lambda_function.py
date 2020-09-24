8703685b3707b4a8fd822b53cf4633ec6bc0d81a
Jordy van Ekelen <jordy.vanekelen@nldm-7zn2.home>
2020-09-24 15:26:54 +0200

    Added..

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -5,10 +5,12 @@ print('Loading function...')
 print('I exisddsssssst!')
 
 def lambda_handler(event, context):
+    """
     print("Received event: " + json.dumps(event, indent=2))
     print("value1 = " + event['key2'])
     print("value2 = " + event['key2'])
     print("value3 = " + event['key3'])
+    """
 
     body = {
         "president": {
