0086d9b3c87473e208017b654ef9cb767ea69fb6
Jordy van Ekelen <jordy.vanekelen@nldm-7zn2.home>
2020-09-24 14:22:42 +0200

    Added..

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -9,6 +9,6 @@ def lambda_handler(event, context):
     print("value1 = " + event['key2'])
     print("value2 = " + event['key2'])
     print("value3 = " + event['key3'])
-    return "Hello World"
+    return '{"key": "val"}'
     # return event['key1']  # Echo back the first key value
     # raise Exception('Something went wrong')
