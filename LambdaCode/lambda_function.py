f7fad3d462a44a07a51eddfb456eb05ba11d60cd
Jordy van Ekelen <jordy.vanekelen@nldm-7zn2.home>
2020-09-24 10:50:33 +0200

    Added lines.

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -2,7 +2,7 @@ import json
 
 print('Loading function...')
 
-print('I exissssst!')
+print('I exist!')
 
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
